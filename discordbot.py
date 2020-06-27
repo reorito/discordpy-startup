9f692cd4edfdf04a02cb31a867f3edbdc76c37ee
reorito <67509424+reorito@users.noreply.github.com>
2020-06-27 23:09:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,8 +3,8 @@ import discord
 from discord.ext import tasks
 from datetime import datetime 
 
-TOKEN = "**********" #トークン
-CHANNEL_ID = ********** #チャンネルID
+TOKEN = "NzI2NDE0NjY5Nzg2NjQ0NDkw.Xvc9Qw.31Vd8SPzhnAv70gG1KWZDQ93UWs" #トークン
+CHANNEL_ID = 704288269998751854 #チャンネルID
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
@@ -13,7 +13,7 @@ client = discord.Client()
 async def loop():
     # 現在の時刻
     now = datetime.now().strftime('%H:%M')
-    if now == '07:00':
+    if now == '23:15':
         channel = client.get_channel(CHANNEL_ID)
         await channel.send('おはよう')  
 
